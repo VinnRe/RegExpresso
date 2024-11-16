40da7208e5da159c7a224d7efec3d9788a3ddec0
Timothy Alimagno <davetimothyy@gmail.com>
2024-11-16 12:44:34 +0800

    added home page and scroll function

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,12 +1,24 @@
 import './styles/Homepage.css'
-import Logo from '../assets/header_regex_only.svg'
+import Logo from '../assets/header_reg_expresso.svg'
+import Header from '../assets/header_regex_only.svg'
 
 const Homepage = () => {
+    const scrollToMainPage = () => document.querySelector('.homeMainPage')?.scrollIntoView({ behavior: 'smooth' });
+
     return (
         <body className="homepageBody">
-            <header className="homepageHeader">
-                <img src={Logo} alt="" className="logoH" />
-            </header>
+            <section className="homeLandingPage containerPage">
+                <header className='mainLogo'>
+                    <img src={Logo} alt="RegExpersso" />                  
+                </header>
+                <button className='buttonStart' onClick={scrollToMainPage}>Start Converting</button>
+            </section>
+
+            <main className='homeMainPage containerPage'>
+                <header>
+                    <img src={Header} alt="RegExpersso" />                  
+                </header>
+            </main>
         </body>
     )
 }
