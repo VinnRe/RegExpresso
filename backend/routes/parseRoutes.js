4e18043b44ba45c1b3fd5f19ce7b518299fea0bc
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-11-24 22:23:59 +0800

    Remove test route

--- backend/routes/parseRoutes.js
+++ backend/routes/parseRoutes.js
@@ -7,7 +7,6 @@ router.route("/NFA").post(parseController.parseNFA);
 router.route("/DFA").post(parseController.parseDFA);
 router.route("/visualizeNFA").post(parseController.visualizeNFA);
 router.route("/visualizeDFA").post(parseController.visualizeDFA);
-router.route("/tuples").post(parseController.to5Tuples);
 router.route("/save").post(userController.protect, parseController.saveRegEx);
 router.route("/delete/:id").delete(userController.protect, parseController.deleteRegEx);
 router.route("/fetchAll").get(userController.protect, parseController.fetchAllRegEx);
