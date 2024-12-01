5bd772d738cc5eb5bfed58f872221445bc8e1189
Timothy Alimagno <davetimothyy@gmail.com>
2024-12-01 11:42:15 +0800

    changed public images

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
             <section className="about__body--left">
             </section>
             <section className="about__body--right">
-                <a href="/home" className="home__button"><img src={RegExpLogo} alt="" className='abt__logo' /> <img src={BackButton} alt="home_button" className='home__btn'/></a>
+                <a href="/home" className="home__button"><img src={RegExpLogo} alt="" className='abt__logo' /> <img src={BackButton} alt="home_button" className='home__btn' /></a>
                 <h1>About Us</h1>
                 <p>RegExpresso is a web application that converts Regular Expressions to Finite State Machines.
                     TripleThreat created this web application for users to visualize their regular expressions in a simple
                     and easy to use interface.
                 </p>
-                <p className="footer_arr">Copyright © 2024 TripleThreat</p>
+                <p className="footer_arr"><br />Copyright © 2024 TripleThreat</p>
             </section>
         </main>
     )
