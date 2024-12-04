8a12107ff9775d63ada72ad7eb965a3091a4ce4b
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-04 23:21:10 +0800

    Redesign styles

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import './styles/About.css'
 import RegExpLogo from '../assets/header_regex_only.svg'
 import BackButton from '../assets/button_back.svg'
+import GithubLogo from '../assets/github.svg'
 
 const About = () => {
     return (
@@ -14,6 +15,13 @@ const About = () => {
                     TripleThreat created this web application for users to visualize their regular expressions in a simple
                     and easy to use interface.
                 </p>
+                <section className="about__contact-links">
+                    <ul className="about__social">
+                        <li><a href="https://github.com/mothy-08"><img src={GithubLogo} alt="githubLink" /> <p>Timothy Alimagno</p></a></li>
+                        <li><a href="https://github.com/VinnRe"><img src={GithubLogo} alt="githubLink" /><p>Kobe Capinpin</p></a></li>
+                        <li><a href="https://github.com/Exuille"><img src={GithubLogo} alt="githubLink" /><p>Alexander Penuliar</p></a></li>
+                    </ul>
+                </section>
                 <p className="footer_arr"><br />Copyright © 2024 TripleThreat</p>
             </section>
         </main>
